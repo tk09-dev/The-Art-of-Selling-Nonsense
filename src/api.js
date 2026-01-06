0a5ccd8863edd78573c57af8361b00fb66b38d3a
tk09-dev <tk.09@gmx.at>
2026-01-06 12:07:36 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5050',
+  baseURL: 'https://the-art-of-selling-nonsense-backend.onrender.com',
 });
 
 export default api;
